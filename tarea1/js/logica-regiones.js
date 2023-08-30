9c4e024a5a16a6e8ccf78400b61d78d72a269453
Benjamin Nicholas Torrealba Troncoso <benjamin.torrealba@ug.uchile.cl>
2023-08-30 14:36:19 -0400

    Initial Commit

---
+++ tarea1/js/logica-regiones.js
@@ -0,0 +1,39 @@
+// recuperamos el diccionario de regiones comunas desde regiones-comunas.js
+
+
+// obtenemos el select de regiones en el html
+let region_select = document.getElementById("region");
+let comunas_select = document.getElementById("comuna");
+
+// agregamos las regiones desde el archivo regiones-comunas.js 
+
+// la estructura del diccionario regionesComunas es {region1: ["comuna1","comuna2"]}
+// por lo que para acceder a las regiones debemos usar Object.keys(regiones_comunas)
+// y para acceder a las comunas de cada region debemos usar regiones_comunas[region]
+
+// iteramos sobre Object.keys(regiones_comunas) agregando las llaves como opciones
+for (let i = 0; i < Object.keys(regionesComunas).length; i++) {
+    let option = document.createElement("option");
+    option.value = Object.keys(regionesComunas)[i];
+    option.text = Object.keys(regionesComunas)[i];
+    region_select.appendChild(option);
+}
+
+
+// agregamos las comunas en funcion de la region seleccionada
+
+// agregamos un event listener al select de regiones
+region_select.addEventListener("change", function () {
+    // obtenemos el valor de la region seleccionada
+    let region = region_select.value;
+    // borramos las opciones anteriores
+    comunas_select.innerHTML = "";
+    // iteramos sobre las comunas de la region seleccionada
+    for (let i = 0; i < regionesComunas[region].length; i++) {
+        let option = document.createElement("option");
+        option.value = regionesComunas[region][i];
+        option.text = regionesComunas[region][i];
+        comunas_select.appendChild(option);
+    }
+}
+);
